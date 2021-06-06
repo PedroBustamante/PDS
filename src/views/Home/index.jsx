350ea7c2d8557f76eca65c31ee5cc2a77075e638
Marco Túlio <marcotmotta@gmail.com>
2021-06-06 18:38:57 -0300

    Add material

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 
 import './styles.scss'
 
+import { Button } from '@material-ui/core';
+
 const Home = () => {
         return (
             <div className="home">
-                <h1>Teste home!</h1>
+                <h1>Salve!</h1>
+                <p>Conecte-se com seu ídolo favorito!</p>
+                <div className="buttons">
+                    <Button className="home-button" variant="outlined">Entrar</Button>
+                    <Button className="home-button" variant="outlined">Cadastrar</Button>
+                </div>
             </div>
         )
 }
